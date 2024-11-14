5e888403703906510fadb255eb7c351d1ca825ba
Marek Capistrak <marek.capistrak@gmail.com>
2024-11-14 16:42:26 +0000

    module three: headles tabs + responsivness

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,55 +1,72 @@
 import React from 'react'
-import { Tabs } from 'expo-router'
-import customColors from '@/constants/colors'
-import colors from '@/constants/colors'
-import { TabBarIcon } from '@/components/TabBarIcon'
+import { View } from 'react-native'
+import { TabList, Tabs, TabSlot, TabTrigger } from 'expo-router/build/ui'
+import classNames from 'classnames'
+import { TabButton } from '@/components/TabButton'
+import { Image } from 'expo-image'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const tabs = (
+  <TabList
+    className={classNames(
+      'py-3 sm:py-6',
+      'px-6 sm:px-8',
+      'mx-8 sm:mx-0',
+      'bottom-safe-offset-2 sm:bottom-safe-offset-0', // keep the tabs above safe ares
+      'rounded-3xl sm:rounded-none', // round the corners
+      'absolute right-0 left-0 sm:relative', // position above content
+      'shadow-md shadow-slate-300', // yum, shadows!
+      'sm:justify-end sm:gap-x-4 sm:shadow-sm',
+      'bg-white'
+    )}
+  >
+    <TabTrigger name="index" href="/" asChild>
+      <TabButton icon="museum">Home</TabButton>
+    </TabTrigger>
+    <TabTrigger name="exhibits" asChild href="/exhibits" reset="always">
+      <TabButton icon="palette">Exhibits</TabButton>
+    </TabTrigger>
+    <TabTrigger name="visit" asChild href="/visit">
+      <TabButton icon="map">Visit</TabButton>
+    </TabTrigger>
+    <TabTrigger name="profile" asChild href="/profile">
+      <TabButton icon="person">Profile</TabButton>
+    </TabTrigger>
+  </TabList>
+)
 
 export default function TabLayout() {
   return (
-    <Tabs
-      sceneContainerStyle={{ backgroundColor: colors.white }}
-      screenOptions={{
-        headerShown: false,
-        lazy: false
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarActiveTintColor: customColors.tint,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="museum" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="exhibits"
-        options={{
-          title: 'Exhibits',
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="palette" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="visit"
-        options={{
-          title: 'Visit',
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="map" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon type="MaterialIcons" name="person" color={color} />
-          )
-        }}
-      />
-    </Tabs>
+    <View className="flex-1">
+      <Tabs className="flex-1 sm:flex-col-reverse">
+        <View className="flex-1">
+          <TabSlot />
+        </View>
+        <LinearGradient
+          pointerEvents="none"
+          // Background Linear Gradient
+          colors={['rgba(255,255,255,0)', 'rgba(255,255,255,1)']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: 200
+          }}
+        />
+        {tabs}
+      </Tabs>
+      <View
+        className={classNames(
+          'hidden sm:inline',
+          'absolute left-6 top-5 h-10 w-52'
+        )}
+      >
+        <Image
+          source={require('@/assets/images/logo.svg')}
+          className="w-full h-full"
+        />
+      </View>
+    </View>
   )
 }
--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -1,25 +1,41 @@
-import colors from "@/constants/colors";
-import { MaterialIcons } from "@expo/vector-icons";
-import { TabTriggerSlotProps } from "expo-router/build/ui";
-import { ComponentProps, Ref, forwardRef } from "react";
-import { Text, Pressable, View } from "react-native";
+import colors from '@/constants/colors'
+import { MaterialIcons } from '@expo/vector-icons'
+import { TabTriggerSlotProps } from 'expo-router/build/ui'
+import { ComponentProps, Ref, forwardRef } from 'react'
+import { Text, Pressable, View } from 'react-native'
 
-type Icon = ComponentProps<typeof MaterialIcons>["name"];
+type Icon = ComponentProps<typeof MaterialIcons>['name']
 
 export type TabButtonProps = TabTriggerSlotProps & {
-  icon?: Icon;
-};
+  icon?: Icon
+}
 
 export const TabButton = forwardRef(function TabButtonComponent(
   { icon, children, isFocused, ...props }: TabButtonProps,
   ref: Ref<View>
 ) {
   return (
-    <Pressable ref={ref} {...props}>
+    <Pressable
+      ref={ref}
+      {...props}
+      className={isFocused ? ' sm:border-b-tint sm:border-b-2' : ''}
+    >
       <View className="justify-between items-center gap-y-1 px-2 flex-col">
-        <MaterialIcons name={icon} size={24} />
-        <Text className={"text-md"}>{children}</Text>
+        <MaterialIcons
+          color={isFocused ? colors.tint : colors.black}
+          name={icon}
+          size={24}
+          className="sm:hidden"
+        />
+        <Text
+          className={
+            'text-sm sm:text-lg' +
+            (isFocused ? ' color-tint sm:color-black' : '')
+          }
+        >
+          {children}
+        </Text>
       </View>
     </Pressable>
-  );
-});
+  )
+})
